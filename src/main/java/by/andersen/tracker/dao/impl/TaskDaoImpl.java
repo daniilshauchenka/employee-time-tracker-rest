56444df0e1a14410c0674156f9e38f55f15296c0
Ritveek <guddu.kr234@gmail.com>
2023-12-06 18:45:55 +0530

    methods of taskdao added

--- src/main/java/by/andersen/tracker/dao/impl/TaskDaoImpl.java
+++ src/main/java/by/andersen/tracker/dao/impl/TaskDaoImpl.java
@@ -1,8 +1,13 @@
 package by.andersen.tracker.dao.impl;
 
 import by.andersen.tracker.dao.ITaskDao;
+import by.andersen.tracker.dao.database.HibernateConfig;
 import by.andersen.tracker.dao.exception.DaoException;
+import by.andersen.tracker.model.Employee;
 import by.andersen.tracker.model.Task;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -10,6 +15,17 @@ public class TaskDaoImpl implements ITaskDao {
 
     @Override
     public void add(Task task) throws DaoException {
+        Transaction transaction = null;
+        try (Session session = HibernateConfig.getSession()) {
+            transaction = session.beginTransaction();
+            session.save(task);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DaoException(ex);
+        }
 
     }
 
@@ -25,7 +41,19 @@ public class TaskDaoImpl implements ITaskDao {
 
     @Override
     public Task getById(int id) throws DaoException {
-        return null;
+        Task task = null;
+        Transaction transaction = null;
+        try (Session session = HibernateConfig.getSession()) {
+            transaction = session.beginTransaction();
+            task = session.get(Task.class, id);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DaoException(ex);
+        }
+        return task;
     }
 
     @Override
